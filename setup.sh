ac86392e6a41a45e10fa4efc03558794e05e1c2c
M Sathishkumar <klmsathish@gmail.com>
2021-03-30 12:08:33 +0530

    setup changes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
-mkdir -p ~/.streamlit/                                               
-echo "\                       
-[server]\n\                       
-port = $PORT\n\                       
-enableCORS = false\n\                       
-headless = true\n\                       
-\n\"
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"klmsathish@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
