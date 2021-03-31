c2201e67d5d9a999993d1c3f658d334dd2ce2fc7
M Sathishkumar <klmsathish@gmail.com>
2021-03-31 18:10:27 +0530

    change req

--- test.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-
-converter = tf.lite.TFLiteConverter.from_saved_model("model_final.hdf5")
-tflite_model = converter.convert()
-open("converted_model.tflite", "wb").write(tflite_model)
