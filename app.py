10a9764a8268d734272ae775c06765d8fef51b8e
M Sathishkumar <klmsathish@gmail.com>
2021-03-31 16:42:13 +0530

    changed layout

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from PIL import Image, ImageOps
 import cv2
 import os 
 import keras
-import tensorflow as tf
 
 basepath = os.path.dirname(__file__)
 MODEL_PATH = 'model_final.hdf5'
@@ -12,7 +11,7 @@ file_path = os.path.join(basepath, MODEL_PATH)
 print(file_path)
 
 # Load your trained model
-model = tf.keras.models.load_model(file_path)        # Necessary
+model = keras.models.load_model(file_path)        # Necessary
 
 
 print('Model loaded. Check http://127.0.0.1:5000/')
@@ -38,12 +37,11 @@ else:
     
     # Make prediction
     result = model.predict_classes(vals)
-
-col1, col2, col3 = st.beta_columns(3)
 col1, col2, col3 , col4, col5 = st.beta_columns(5)
 with col3 :
-    if st.button('Predict',key = 1):
-        if result[0][0] == 1 :
-            st.error("OMG! You have cataract")
-        if result[0][0] == 0 :
-            st.success("YAYYY !! You are very fine")
+    button = st.button('Predict',key = 1)
+if button :
+    if result[0][0] == 1 :
+        st.error("You have cataract")
+    if result[0][0] == 0 :
+        st.success("You are fine")
